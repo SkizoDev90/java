85275b0bf177a197e1d39c6f89022a04cc9173ac
SkizoX <franciscomanuelcarrillofernandez.guadalupe@alumnado.fundacionloyola.net>
2025-04-11 14:08:38 +0200

    Add files via upload

--- Libre.java
+++ Libre.java
@@ -19,24 +19,39 @@ public class Libre extends JFrame {
 	
 	public void inicializar(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
+		setBounds(200, 200, 800, 600);
+		setResizable(false);
 	}
 	public void paint (Graphics g) {
 	
 		super.paint (g);
-		/////////////////////////////////////valla arriba
+		/////////////////////////////////////rio arriba
+		g.setColor (new Color(207, 243, 241 )); 
+		int [] vxl = {-150, 800, 800};  	//arriba, derecha, izquierda
+		int [] vyl = {0, 0, 200}; 	//arriba, derecha, izquierda
+		g.fillPolygon (vxl, vyl, 3);
+		////////////////////////////////////linea arriba
 		g.setColor (Color.black); 
-		g.drawLine (0,80,900,350);
-		g.setColor (Color.black); 
+		g.drawLine (-150,0,800,200); 
+		
+		/*g.setColor (Color.black); 
 		g.drawLine (0,160,900,430);
 		/////////////////////////////////////valla abajo
 		g.setColor (Color.black); 
 		g.drawLine (0,270,900,560); 
 		
 		g.setColor (Color.black); 
-		g.drawLine (0,450,900,750);
+		g.drawLine (0,450,400,600);*/
+		////////////////////////////////////linea arriba
+		g.setColor (Color.black); 
+		g.drawLine (0,450,800,650);
 		
-		 
+		////////////////////////////////////// rio abajo
+		g.setColor (new Color(207, 243, 241 )); 
+		int [] vxl1 = {0, 0, 800};  	//arriba, derecha, izquierda
+		int [] vyl1 = {450,600, 650}; 	//arriba, derecha, izquierda
+		g.fillPolygon (vxl1, vyl1, 3);
+		
 		
 	
 	}
